5f79a36e8f08ea97bf552968f6a381cd2e6ccc1b
Bryan Ivory <bivory@gmail.com>
2012-02-23 12:27:26 -0800

    Added vimclojure to the dev depends

--- project.clj
+++ project.clj
@@ -3,4 +3,11 @@
   :dependencies [[org.clojure/clojure "1.3.0"]
                  [koan-engine "0.1.2-SNAPSHOT"]]
   :dev-dependencies [[swank-clojure "1.3.0" :exclusions [org.clojure/clojure]]
-                     [lein-koan "0.1.0"]])
+                     [lein-koan "0.1.0"]
+
+                     ;; Vim
+                     ;; https://bitbucket.org/kotarak/vimclojure
+                     [vimclojure/server "2.3.1"]
+                     [org.clojars.autre/lein-vimclojure "1.0.0"]]
+
+  :project-init (require 'clojure.pprint))
